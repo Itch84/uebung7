05ba4d52eafb1ceeccca4c96e395b6eeb3d632ff
Itch84 <itch84@gmail.com>
2013-12-02 22:10:50 +0100

    Home-Page ver??ndert

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Uebung7::Application.routes.draw do
   resources :tasks
-
+  root 'tasks#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
