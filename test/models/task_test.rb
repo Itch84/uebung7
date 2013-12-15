2bee12c8335f04e8ba64618078175a15924017e2
Itch84 <itch84@gmail.com>
2013-12-15 12:23:57 +0100

    distance_from_now_in_days Methode in Task eingefuegt

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -36,4 +36,22 @@ class TaskTest < ActiveSupport::TestCase
    assert !task.is_delayed?
   end
   
+  
+  test "distance in days from today" do
+    task = Task.new
+    task.deadline = Date.today
+    assert_equal 0, task.distance_from_now_in_days
+  end
+  
+  test "destance in days from yesterday" do
+    task = Task.new
+    task.deadline = Date.today - 1
+    assert_equal -1, task.distance_from_now_in_days
+  end
+  
+  test "destance in days from tomorrow" do
+    task = Task.new
+    task.deadline = Date.today + 1
+    assert_equal 1, task.distance_from_now_in_days
+  end 
 end
