ebe5037e501657adbaa080a42a0aa03cd0a44449
Itch84 <itch84@gmail.com>
2013-12-15 12:17:37 +0100

    is_delayed? Methode in Task eingef??gt

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,13 @@
 class Task < ActiveRecord::Base
 
-validates :name, presence: true
-validates :deadline, presence: true
-validates :duration, presence: true, numericality: true 
+  validates :name, presence: true
+  validates :deadline, presence: true
+  validates :duration, presence: true, numericality: true 
+  
+  def is_delayed?
+   self.deadline < Date.today
+  end
+
 
 
 end
